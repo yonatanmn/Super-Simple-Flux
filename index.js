e5ad7ec0cf8f3411d3cd80f5b0fb4780756e7faa
Zheng  Weihan <weihan@wscouting.com>
2015-08-19 16:51:23 +0300

    added shouldStoreUpdate method to mixin. This allows user to define a method 'shouldStoreUpdate', which can be used to skip state/store triggers and componentDidUpdate after state updates, by returning false from this method. This is similar to shouldComponentUpdate in React.

--- index.js
+++ index.js
@@ -11,84 +11,101 @@ var utils = require('./utils.js');
  */
 module.exports = function stateMixin(Reflux) {
 
-  if (typeof Reflux !== 'object' || typeof Reflux.createAction !== 'function') {
-    throw new Error('Must pass reflux instance to reflux-state-mixin');
-  }
-
-  function attachAction(options, actionName) {
-    if (this[actionName]) {
-      console.warn(
-          'Not attaching event ' + actionName + '; key already exists'
-      );
-      return;
+    if (typeof Reflux !== 'object' || typeof Reflux.createAction !== 'function') {
+        throw new Error('Must pass reflux instance to reflux-state-mixin');
     }
-    this[actionName] = Reflux.createAction(options);
-  }
 
-  return {
-    setState: function (state) {
-      var changed = false;
-      var prevState = update({}, {$merge: this.state});
-
-      for (var key in state) {
-        if (state.hasOwnProperty(key)) {
-          if (this.state[key] !== state[key]) {
-            this[key].trigger(state[key]);
-            changed = true;
-          }
+    function attachAction(options, actionName) {
+        if (this[actionName]) {
+            console.warn(
+                'Not attaching event ' + actionName + '; key already exists'
+            );
+            return;
         }
-      }
+        this[actionName] = Reflux.createAction(options);
+    }
 
-      if (changed) {
-        this.state = update(this.state, {$merge: state});
-
-        if (utils.isFunction(this.storeDidUpdate)) {
-          this.storeDidUpdate(prevState);
-        }
-
-        this.trigger(this.state);
-      }
-
-    },
-
-    init: function () {
-      if (utils.isFunction(this.getInitialState)) {
-        this.state = this.getInitialState();
-        for (var key in this.state) {
-          if (this.state.hasOwnProperty(key)) {
-            attachAction.call(this, this.state[key], key);
-          }
-        }
-      }
-    },
-
-    connect: function (store, key) {
-      return {
-        getInitialState: function () {
-          if (!utils.isFunction(store.getInitialState)) {
-            return {};
-          } else if (key === undefined) {
-            return store.state;
-          } else {
-            return utils.object([key], [store.state[key]]);
-          }
-        },
-        componentDidMount: function () {
-          utils.extend(this, Reflux.ListenerMethods);
-          var noKey = key === undefined;
-          var me = this,
-              cb = (noKey ? this.setState : function (v) {
-                if (typeof me.isMounted === "undefined" || me.isMounted() === true) {
-                  me.setState(utils.object([key], [v]));
+    return {
+        setState: function (newState) {
+            var changed = false;
+            var prevState = update({}, {$merge: this.state});
+            var statesToTrigger = [];
+            for (var key in newState) {
+                if (newState.hasOwnProperty(key)) {
+                    if (this.state[key] !== newState[key]) {
+                        this.state[key] = newState[key];
+                        statesToTrigger.push(key);
+                        //this[key].trigger(state[key]);
+                        changed = true;
+                    }
                 }
-              }),
-              listener = noKey ? store : store[key];
-          this.listenTo(listener, cb);
+            }
+
+            if (changed) {
+                if(utils.isFunction(this.shouldStoreUpdate)) {
+                    if(this.shouldStoreUpdate(prevState) === false){
+                        return;
+                    }
+                }
+
+                for(var index in statesToTrigger){
+                    key = statesToTrigger[index];
+                    this[key].trigger(newState[key]);
+                }
+
+                this.trigger(this.state);
+
+                if (utils.isFunction(this.storeDidUpdate)) {
+                    this.storeDidUpdate(prevState);
+                }
+            }
+
         },
-        componentWillUnmount: Reflux.ListenerMixin.componentWillUnmount
-      }
+
+        init: function () {
+            if (utils.isFunction(this.getInitialState)) {
+                this.state = this.getInitialState();
+                for (var key in this.state) {
+                    if (this.state.hasOwnProperty(key)) {
+                        attachAction.call(this, this.state[key], key);
+                    }
+                }
+            }
+        },
+
+        connect: function (store, key) {
+            return {
+                getInitialState: function () {
+                    if (!utils.isFunction(store.getInitialState)) {
+                        return {};
+                    } else if (key === undefined) {
+                        return store.state;
+                    } else {
+                        return utils.object([key], [store.state[key]]);
+                    }
+                },
+                componentDidMount: function () {
+                    utils.extend(this, Reflux.ListenerMethods);
+                    var noKey = key === undefined;
+                    var me = this,
+                        cb = (noKey ? this.setState : function (v) {
+                            if (typeof me.isMounted === "undefined" || me.isMounted() === true) {
+                                me.setState(utils.object([key], [v]));
+                            }
+                        }),
+                        listener = noKey ? store : store[key];
+                    this.listenTo(listener, cb);
+                },
+                componentWillUnmount: Reflux.ListenerMixin.componentWillUnmount
+            }
+        }
     }
-  }
 };
+
+
+
+
+
+
 
 
